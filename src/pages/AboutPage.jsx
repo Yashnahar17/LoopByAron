b363abe1dd80a40d2690162570c82d3708a1aef8
Yashnahar17 <yash.nahar1709@gmail.com>
2026-04-12 13:03:47 +0530

    syntax change

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion'
 import { AtSign, Instagram, Mail, Quote } from 'lucide-react'
-import aronImg from "../assets/images/aron.jpg"
+import aron from "../assets/images/aron.jpg"
 import FadeIn from '../components/FadeIn'
 import Footer from '../components/Footer'
 
